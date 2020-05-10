6a4bb16f3d5ee206b103e83ac0299b59546d0ec5
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-05-10 18:09:26 +0900

    userコントローラーの記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,25 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find(params[:id])
-    @name = user.name
-    @posts = user.posts.page(params[:page]).per(5).order("created_at DESC")
+    @user = User.find(params[:id])
+    @name = @user.name
+    @posts = @user.posts.page(params[:page]).per(5).order("created_at DESC")
+    @currentUserEntry=Entry.where(user_id: current_user.id) # 現在ログインしているユーザー情報
+    @userEntry=Entry.where(user_id: @user.id) #相手方のユーザー情報
+    unless @user.id == current_user.id #現在ログインしてるユーザーではない時
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then #相手方とのルームが既に作成されているか
+            @isRoom = true 
+            @roomId = cu.room_id #room_idを特定
+          end
+        end
+      end
+      if @isRoom
+      else #roomがまだ作成されていない場合
+        @room = Room.new #新規roomの作成
+        @entry = Entry.new
+      end
+    end
+   end
   end
-
-end
