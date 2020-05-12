54b34ae6bff1e614c8cbe8927bea655a3a029eba
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-05-12 16:42:59 +0900

    post.rb/ after_createの記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,14 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :post_tags
   has_many :tags, through: :post_tags
-
   mount_uploader :image, ImageUploader
+
+  after_create do
+    post = Post.find_by(id: self.id)
+    tags = self.text.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
+    tags.uniq.map do |t|
+      tag = Tag.find_or_created_by(text: t.downcase.delete('#'))
+      post.tags << tag
+    end
+  end
 end
