26086d7ce29d06a69b14d21ddc8f0ea203926987
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-05-19 12:30:21 +0900

    change_post_show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.create(comment_params)
-    redirect_to  "/posts/#{comment.post.id}"
+    redirect_to "/posts/#{comment.post.id}"
   end
 
   private
