93803e89ab3bc966f682d54193f64b9fedd4dfed
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-05-24 23:53:45 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,10 +34,10 @@ class PostsController < ApplicationController
 
   def search
     @posts = Post.search(params[:keyword]).order("created_at DESC").page(params[:page]).per(5)
-    respond_to do |format|
-      format.html
-      format.json
-    end
+    # respond_to do |format|
+    #   format.html
+    #   format.json
+    # end
   end
   
 
