4a084a89f1e6a77b66775d262eafe49599d0fa83
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-06-05 19:32:22 +0900

    プレビュー表示まで

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,10 +37,6 @@ class PostsController < ApplicationController
 
   def search
     @posts = Post.search(params[:keyword]).order("created_at DESC").page(params[:page]).per(5)
-    # respond_to do |format|
-    #   format.html
-    #   format.json
-    # end
   end
   
 
