4fba4466343501762ecfa02105b81d4fd49d73e4
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-06-11 13:44:26 +0900

    いいね機能実装
    posts_showに実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :move_to_index, except: [:index, :show, :search]
 
   def index 
+    @like = Like.new
     if params[:tag_id]
       @select_tag = Tag.find(params[:tag_id])
       @posts = Post.from_tag(params[:tag_id]).order("created_at DESC").page(params[:page]).per(5)
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :new, :create, :show, :destroy] do
     resources :likes, only: [:create, :destroy] 
-    resources :comments, only: :createl
+    resources :comments, only: :create
     collection do
       get 'search'
     end
