5dec39895578c3a4ef5d44301211e5bb417e5f6a
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-06-17 20:37:15 +0900

    ajax定義

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -3,5 +3,14 @@ $(function(){
     e.preventDefault();
     console.log(this)
     const formData = new FormData(this);
+    const url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: 'formdata',
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
 })
