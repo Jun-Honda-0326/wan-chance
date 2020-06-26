f4b3edcdbfb5eb200c0aeca29b37187dace18c92
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-06-26 17:33:11 +0900

    ajaxの設定

--- app/assets/javascripts/channels/message.js
+++ app/assets/javascripts/channels/message.js
@@ -1,6 +1,16 @@
 $(function(){
   $('#new_message').on('submit', function(e){
     console.log('OK');
-    // e.preventDfault()
+    e.preventDefault()
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url:url,
+      type: 'POST',
+      data:FormData,
+      processData:false,
+      contentType: false
+    })
   });
+
 });
