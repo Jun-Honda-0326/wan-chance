ce9ed243acb4e38461ca139390991de8cd038008
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-06-26 18:48:08 +0900

    非同期処理の実装

--- app/assets/javascripts/channels/message.js
+++ app/assets/javascripts/channels/message.js
@@ -1,16 +1,42 @@
 $(function(){
+
+  function buildHTML(message){
+    var html = `<div class = "message">
+                  <div class= "user-info" >
+                    <div class= "user-info__name">
+                      ${message.user_name}
+                    </div>
+                    <div class= "user-info__time">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class = "message__text">
+                    ${message.message}
+                  </div>
+                </div>`
+            return html;
+}
+
   $('#new_message').on('submit', function(e){
-    console.log('OK');
-    e.preventDefault()
+    e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
-      url:url,
-      type: 'POST',
-      data:FormData,
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
       processData:false,
       contentType: false
     })
-  });
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main-chat__messages').append(html);
+      $('.main-chat__messages').animate({ scrollTop: $('.main-chat__messages')[0].scrollHeight});
+      $('form')[0].reset();
+    })
+    
 
+
+  });
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,11 +5,12 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-      respond_to |format|
+      respond_to do |format|
         format.json
       end
     else
       redirect_to "/rooms/#{@message.room_id}", alert: "メッセージを入力してください"
+    end
   end
 
   def message_params
