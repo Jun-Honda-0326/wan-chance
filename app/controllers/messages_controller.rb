49bb57d6a806b1fd8a796ad3e6785d10bacb0f40
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-06-26 18:13:26 +0900

    json条件分岐

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,14 +3,17 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
   def create
-    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
-      @message = Message.create(params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
+    @message = Message.new(message_params)
+    if @message.save
+      respond_to |format|
+        format.json
+      end
     else
-      flash[:alert] = "メッセージ送信に失敗しました。"
-    end
-    redirect_to "/rooms/#{@message.room_id}"
+      redirect_to "/rooms/#{@message.room_id}", alert: "メッセージを入力してください"
   end
 
-
+  def message_params
+    params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id)
+  end
 
 end
